c02a3d71c9a7a243525094c4a75d20033dbac180
Lin Zhang <lin.zhang.ux@gmail.com>
2019-03-22 21:44:14 -0400

    fix mixed content error

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   const win = window
   const doc = document.documentElement
 
@@ -12,7 +12,7 @@
   win.addEventListener('load', fixHeroBgHeight)
   win.addEventListener('resize', fixHeroBgHeight)
 
-  function fixHeroBgHeight () {
+  function fixHeroBgHeight() {
     const bodyHeight = doc.getElementsByTagName('body')[0].clientHeight
     heroDesktopBg.style.height = `${bodyHeight}px`
   }
@@ -47,5 +47,33 @@
       origin: 'right',
       interval: 150
     })
+  }
+
+  //firebase.auth().onAuthStateChanged(function(){})
+  if ($('#new-contact').length > 0) {
+    contactScript('forcontact');
+  }
+
+  function contactScript(value) {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyCJ_aB6KdLpKjhLe29dABClw8_PtD_uSDg",
+      authDomain: "test-67717.firebaseapp.com",
+      databaseURL: "https://test-67717.firebaseio.com",
+      projectId: "test-67717",
+      storageBucket: "test-67717.appspot.com",
+      messagingSenderId: "1008090308069"
+    };
+    firebase.initializeApp(config);
+    var db = firebase.database().ref("email");
+
+    $("#new-contact").submit(function(config) {
+      $(this), console.log("Submit to Firebase");
+      var email = document.getElementById("email").value;
+      return db.push(email).then(function(config) {
+        $(".success").css("display", "block"),
+          $(".success-none").css("display", "none")
+      }), !1
+    });
   }
 }())
